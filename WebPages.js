d9a7487a428bf05c2839cb0e27f2202a4f6d4bc7
Tya Meilisa <117277043+tyameilisa@users.noreply.github.com>
2025-11-08 22:18:34 +0700

    Create WebPages.js

---
+++ WebPages.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html> //what is all this??
+  //it tells the browser how to interpret the rest of the file
+  <html>
+
+  <!-- if I want to comment I shall use this -->
+  
+  <body>
+  //the tag defines whats visible on the page (what user see)
+  <h1> HEWWOOO?? </h1>  
+  //header 1 is the biggest title text. apparenbtly i dont need quotes
+  <h2> i just write whatever, lol </h2> 
+  // header two is smaller. dont forget the slash at the second <>
+  
+    <p id="demo" ></p>  
+    
+  //bruh what was that?
+    let result = myFunction(4,3);
+    document.getElementByID("demo").innerHTML = "The product is:" +result;
+
+</script> 
+  //what is this?
+  </body>
+  </html> 
+  // why the slash? this is the closing of that <html> and >body> above
+
+  
