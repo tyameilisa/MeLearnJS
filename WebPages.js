f3033f5756dd0587552f3017c9c937c7326736a8
tyameilisa <117277043+tyameilisa@users.noreply.github.com>
2025-11-08 22:41:49 +0700

    Update WebPages.js

--- WebPages.js
+++ WebPages.js
@@ -4,7 +4,7 @@
     <h2> I think i know how this works, lol</h2>
       <p id= "demo"> </p> 
 
-      <script>
+      <script src="WebPage.js">
         function myFunction(a,b){
           return a*b;
         }
@@ -16,6 +16,6 @@
         "how am I making sense of this functions?"
         "why not make 2 times 3 easy? why must the return and let result? so complicated"
         "can't express my frustration with emoji either!"
-      </script>
+      </script> 
   </body>
 </html>
